8beb07ceaad1a1395c94e21c8be24371acea55dc
Jay Asbury <vbjay.net@gmail.com>
2017-10-21 15:34:28 -0400

    Add Person data model class and data source

---
+++ DatagridView GetData/Person.vb
@@ -0,0 +1,6 @@
+﻿Public Class Person
+    Property Name As String = ""
+    Property EmailAddress As String = ""
+    Property Birthdate As Date = Now.Date.AddYears(-18).AddMonths(-3).AddDays(-12)
+
+End Class
