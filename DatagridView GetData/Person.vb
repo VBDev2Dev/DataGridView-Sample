75938af08bd6a3896e1c3aee03514774032ec5f2
Jay Asbury <vbjay.net@gmail.com>
2017-10-21 16:32:37 -0400

    Make MessageBox show details

--- DatagridView GetData/Person.vb
+++ DatagridView GetData/Person.vb
@@ -2,5 +2,7 @@
     Property Name As String = ""
     Property EmailAddress As String = ""
     Property Birthdate As Date = Now.Date.AddYears(-18).AddMonths(-3).AddDays(-12)
-
+    Public Overrides Function ToString() As String
+        Return $"Name:{Name} Birthday:{Birthdate.ToShortDateString} Email:{EmailAddress}"
+    End Function
 End Class
